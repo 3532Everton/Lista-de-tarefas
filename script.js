54247d9a0b514fd5b1665061483ba0d50c318bc7
Everton Martins <3532everton@gmail.com>
2022-04-05 15:29:59 -0300

    começar a fazer indentificadores para exclusão

--- script.js
+++ script.js
@@ -36,19 +36,26 @@ function textColor(cor){
     }
 }
 // -------------------------------------------------------
-// Script para adicionar as tarefas
+// Botão limpar tarefas
 
-function adicionarTarefa(){
+const bntLimpar = document.getElementById('bnt-clear-tarefas')
+bntLimpar.addEventListener('click', function(){
+    const valorInput = document.getElementById('titulo-tarefa')
+    const valorText = document.getElementById('corpo-tarefa')
+
+    // Limpando os dados da tela
+    valorInput.value = ''
+    valorText.value = ''
+})
+
+// Script para manipular as tarefas as tarefas
+function manipularTarefas(){
     // Recebendo o valores do titulo e corpo
     const valorInput = document.getElementById('titulo-tarefa')
     const tituloTarefa = valorInput.value
     const valorText = document.getElementById('corpo-tarefa')
     const corpoTarefa = valorText.value
     const addTarefas = document.getElementById('tarefas')
-
-    // Limpando os dados da tela
-    valorInput.value = ''
-    valorText.value = ''
 
     // Verificando se há algo para adicionar
     if (tituloTarefa === "" && corpoTarefa === ""){
@@ -66,55 +73,76 @@ function adicionarTarefa(){
         const blocoTarefa = document.createElement("div") // cria o elemento 'div'
         const bTitulo = document.createElement("h3") // cria o elemento 'h3'
         const bCorpo = document.createElement('p') // cria o elemento 'p'
+        const bntExcluir = document.createElement('button') // cria o elemento 'button'
+        const iconLixo = document.createElement('i') // cria o elemento 'i'
         
         blocoTarefa.classList.add('bloco-tarefa') // adicioan a class no elemento 'div'
         bTitulo.classList.add('bTitulo') // adiciona a classe no elemento 'h3'
         bCorpo.classList.add('bCorpo') // adiciona a classe no elemento 'p'
+        bntExcluir.classList.add('bnt-excluir') // adiciona a classe no elemento 'button'
+        iconLixo.classList.add('fa', 'fa-trash') // adiciona a classe no elemento 'i'
 
         addTarefas.insertAdjacentElement("afterbegin", blocoTarefa) // adiciona a div na div.tarefas
         blocoTarefa.insertAdjacentElement('afterbegin', bTitulo) // adiciona o elemento 'h3' com o titulo na div
         blocoTarefa.insertAdjacentElement('beforeend', bCorpo) // adiciona o elemento 'p' com o corpo na div
+        blocoTarefa.insertAdjacentElement('beforeend', bntExcluir) // adiciona o elemento 'button' com a função de excluir no div
+        bntExcluir.insertAdjacentElement('beforeend', iconLixo) // adiciona o elemento 'i' com o icone de lixeira
 
         const titulo = document.querySelector('.bTitulo')
         titulo.innerHTML = tarefa.titulo // adiciona o texto do titulo
         
         const corpo = document.querySelector('.bCorpo')
         corpo.innerHTML = tarefa.corpo // adiciona o texto do corpo
+        corpo.style.textAlign = alinhamento
+        corpo.style.fontWeight = negrito
+        corpo.style.fontStyle = italico
 
+        // Limpando os dados da tela
+        valorInput.value = ''
+        valorText.value = ''
     }
+    // Excluir tarefas
+
+    const bntExcluir = document.querySelector('.bnt-excluir')
+    bntExcluir.addEventListener('click', function(){
+        const blocoTarefa = document.querySelector('.bloco-tarefa')
+        blocoTarefa.parentNode.removeChild(blocoTarefa)
+    })
 }
+// -------------------------------------------------------
 
 // botôes de estilização da tarefa
 const bntNegrito = document.getElementById("bnt-negrito")
 const bntItalico = document.getElementById("bnt-italico")
+const bntJustificar = document.getElementById("bnt-justificar")
+const bntCentralizar = document.getElementById("bnt-centralizar")
+const bntEsquerda = document.getElementById("bnt-esquerda")
+const bntDireita = document.getElementById("bnt-direita")
+const corpoTarefa = document.getElementById("corpo-tarefa")
+let alinhamento = ""
+let negrito = ""
+let italico = ""
 
 bntNegrito.addEventListener("click", function(){
-    const corpoTarefa = document.getElementById("corpo-tarefa")
-
     if(corpoTarefa.style.fontWeight == "700") {
         corpoTarefa.style.fontWeight = "normal"
+        negrito = 'normal'
     } else {
         corpoTarefa.style.fontWeight = "700"
+        negrito = '700'
     }
 })
 bntItalico.addEventListener("click", function(){
-    const corpoTarefa = document.getElementById("corpo-tarefa")
-
     if(corpoTarefa.style.fontStyle == "italic"){
-        corpoTarefa.style.fontStyle = "normal"    
+        corpoTarefa.style.fontStyle = "normal"   
+        italico = "normal" 
     } else {
         corpoTarefa.style.fontStyle = "italic"
+        italico = "italic"
     }
 })
+bntJustificar.addEventListener("click", function(){corpoTarefa.style.textAlign = 'justify'; alinhamento = 'justify'})
+bntCentralizar.addEventListener("click", function(){corpoTarefa.style.textAlign = 'center'; alinhamento = 'center'})
+bntEsquerda.addEventListener("click", function(){corpoTarefa.style.textAlign = 'left'; alinhamento = 'left'})
+bntDireita.addEventListener("click", function(){corpoTarefa.style.textAlign = 'right'; alinhamento = 'right'})
 
-// Botão limpar tarefas
-
-const bntLimpar = document.getElementById('bnt-clear-tarefas')
-bntLimpar.addEventListener('click', function(){
-    const valorInput = document.getElementById('titulo-tarefa')
-    const valorText = document.getElementById('corpo-tarefa')
-
-    // Limpando os dados da tela
-    valorInput.value = ''
-    valorText.value = ''
-})
